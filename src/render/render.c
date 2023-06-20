e27750544613f3bfe024864f89cc9bbdd2dfdb9b
Alvaro Lopez <al7aro@gmail.com>
2023-06-20 15:12:03 +0200

    render norm

--- src/render/render.c
+++ src/render/render.c
@@ -6,11 +6,27 @@
 /*   By: alopez-g <alopez-g@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 00:12:11 by alopez-g          #+#    #+#             */
-/*   Updated: 2023/06/20 13:41:14 by alopez-g         ###   ########.fr       */
+/*   Updated: 2023/06/20 15:11:38 by alopez-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
+
+void	draw_map_tile(t_scene *s, t_img *img, int x, int y)
+{
+	if (s->map[y][x] == '1')
+	{
+		draw_rect(img, (t_rect){x * MAP_TILE_SIZE, y * MAP_TILE_SIZE,
+			MAP_TILE_SIZE, MAP_TILE_SIZE}, rbg_to_int(255, 0, 0));
+		draw_rect_border(img,
+			(t_rect){x * MAP_TILE_SIZE, y * MAP_TILE_SIZE,
+			MAP_TILE_SIZE, MAP_TILE_SIZE}, rbg_to_int(0, 0, 0));
+	}
+	if (x == 0 || y == 0
+		|| x == s->map_size.x - 1 || y == s->map_size.y - 1)
+		draw_rect(img, (t_rect){x * MAP_TILE_SIZE, y * MAP_TILE_SIZE,
+			MAP_TILE_SIZE, MAP_TILE_SIZE}, rbg_to_int(0, 0, 0));
+}
 
 int	render_minimap(t_scene *s, int type)
 {
@@ -19,29 +35,14 @@ int	render_minimap(t_scene *s, int type)
 	int		j;
 	t_vec2	pos;
 
-	j = 0;
+	j = -1;
 	img = &s->mlx->img[type];
 	draw_background(img, rbg_to_int(150, 150, 150));
-	while (j < s->map_size.y)
+	while (++j < s->map_size.y)
 	{
-		i = 0;
-		while (i < s->map_size.x)
-		{
-			if (s->map[j][i] == '1')
-			{
-				draw_rect(img, (t_rect){i * MAP_TILE_SIZE, j * MAP_TILE_SIZE,
-					MAP_TILE_SIZE, MAP_TILE_SIZE}, rbg_to_int(255, 0, 0));
-				draw_rect_border(img,
-					(t_rect){i * MAP_TILE_SIZE, j * MAP_TILE_SIZE,
-					MAP_TILE_SIZE, MAP_TILE_SIZE}, rbg_to_int(0, 0, 0));
-			}
-			if (i == 0 || j == 0
-				|| i == s->map_size.x - 1 || j == s->map_size.y - 1)
-				draw_rect(img, (t_rect){i * MAP_TILE_SIZE, j * MAP_TILE_SIZE,
-					MAP_TILE_SIZE, MAP_TILE_SIZE}, rbg_to_int(0, 0, 0));
-			i++;
-		}
-		j++;
+		i = -1;
+		while (++i < s->map_size.x)
+			draw_map_tile(s, img, i, j);
 	}
 	pos = (t_vec2){.x = s->player.pos.x / (TILE_SIZE / MAP_TILE_SIZE),
 		.y = s->player.pos.y / (TILE_SIZE / MAP_TILE_SIZE)};
